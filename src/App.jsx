4972aac912e803b3dc6dc6e4a9e5005534d3ac5e
Muhammad Jahanzeb <i.am.jahanzeb6@gmail.com>
2025-05-05 21:22:22 +0500

    Add assets and install tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        <div className="pattern" />
+        <header className="wrapper">
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without Hassle.
+          </h1>
+          <p>search</p>
+        </header>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
