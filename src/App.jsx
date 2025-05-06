0502b0e2a3f6b3511cd4bea7026c97b3a517e74f
Muhammad Jahanzeb <i.am.jahanzeb6@gmail.com>
2025-05-06 19:36:23 +0500

    Add banner and logo for movies

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ const App = () => {
       <main>
         <div className="pattern" />
         <header className="wrapper">
+          <img src="./logo.png" alt="Logo Image" className="h-20 w-20" />
+          <img src="./hero.png" alt="Hero Banner" />
           <h1>
             Find <span className="text-gradient">Movies</span> You'll Enjoy
             Without Hassle.
